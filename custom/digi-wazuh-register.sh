669214bda4dd50902e6b573c28c7116ae4c27242
Madhurendra Sachan <madhurendra@tikaj.com>
2017-08-19 10:56:17 +0530

    removed useless files.

--- custom/digi-wazuh-register.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-sudo service ossec stop
-sudo /var/ossec/bin/agent-auth -m 127.0.0.1 -p 1515 -A random
-sudo service ossec start
--- custom/tikle.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-node /mnt/micro/Dropbox/Projects/tikle/cli
